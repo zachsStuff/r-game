ee982459fdd3652c3fc19fc3477b7399bc4f4384
zachsStuff <joebertty45@gmail.com>
2026-08-20 09:01:25 -0700

    did stuff

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("game_page.html")
+    name = request.args.get("name", "jessica")
+    Rname = request.args.get("rapist", "tickles")
+    return render_template("game_page.html",name=name,rapist=Rname)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
